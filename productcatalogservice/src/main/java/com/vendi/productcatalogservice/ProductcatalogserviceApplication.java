1f6ccdbbbe16c5c845bd0372bf3ce72408905ab6
srikant19 <srikant.suman19@gmail.com>
2025-02-26 02:16:08 +0530

    vendi: productcatalogservice

---
+++ productcatalogservice/src/main/java/com/vendi/productcatalogservice/ProductcatalogserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vendi.productcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductcatalogserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductcatalogserviceApplication.class, args);
+	}
+
+}
---
+++ productcatalogservice/src/test/java/com/vendi/productcatalogservice/ProductcatalogserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendi.productcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcatalogserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
