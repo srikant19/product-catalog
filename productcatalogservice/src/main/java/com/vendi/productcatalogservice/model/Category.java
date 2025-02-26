242a3eb650427a9f43034d671d604d6979730fc5
srikant19 <srikant.suman19@gmail.com>
2025-02-27 00:41:57 +0530

    Category product entity added

---
+++ productcatalogservice/src/main/java/com/vendi/productcatalogservice/model/Category.java
@@ -0,0 +1,23 @@
+package com.vendi.productcatalogservice.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String name;
+	private String description;
+
+	@OneToMany(mappedBy = "category")
+	private List<Product> products;
+}
---
+++ productcatalogservice/src/main/java/com/vendi/productcatalogservice/model/Product.java
@@ -0,0 +1,36 @@
+package com.vendi.productcatalogservice.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+
+
+@Entity
+public class Product {
+
+	//The @Id annotation is part of the Java Persistence API (JPA), which is used to specify the primary key of an entity in Spring Boot applications. To use @Id, you need to include the JPA dependency in your project.
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stockQuantity;
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private Double rating;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
